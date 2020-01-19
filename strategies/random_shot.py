20891b3771b3e0c02eb86505d61def134ec86017
JT W <jwolohan@gmail.com>
2020-01-19 14:22:51 -0500

    FIX random shot

--- strategies/random_shot.py
+++ strategies/random_shot.py
@@ -2,7 +2,7 @@ from random import randint
 
 def random_shot(board):
     while True:
-        x = randint(0,9)
-        y = randint(0,9)
+        x = randint(0,len(board)-1)
+        y = randint(0,len(board)-1)
         if board[x][y] not in ["X","-"]:
             return x,y
